d2617ca5dc297d5d68862bc67f9f7ede87ef01e3
motzel <motzel@o2.pl>
2024-06-06 20:54:02 +0200

    change default theme to catppuccin

--- install/app-neovim.sh
+++ install/app-neovim.sh
@@ -4,5 +4,5 @@ sudo apt install -y neovim
 
 if [ ! -d "$HOME/.config/nvim" ]; then
 	git clone https://github.com/LazyVim/starter ~/.config/nvim
-	cp ~/.local/share/omakub/themes/neovim/tokyo-night.lua ~/.config/nvim/lua/plugins/theme.lua
+	cp ~/.local/share/omakub/themes/neovim/catppuccin.lua ~/.config/nvim/lua/plugins/theme.lua
 fi
