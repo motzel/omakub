7b6f1390549e2a5035ccc0a76724b8de7e93a692
motzel <motzel@o2.pl>
2024-06-06 21:13:09 +0200

    disable pulling mysql and redis docker images

--- install/docker.sh
+++ install/docker.sh
@@ -8,5 +8,5 @@ curl -sSL https://github.com/docker/compose/releases/download/v$DOCKER_COMPOSE_V
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
 # FIXME: Add postgresql as a default configured DB as well
-sudo docker create --restart unless-stopped -p 3306:3306 --name=mysql8 -e MYSQL_ROOT_PASSWORD= -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql:8
-sudo docker create --restart unless-stopped -p 6379:6379 --name=redis redis
+#sudo docker create --restart unless-stopped -p 3306:3306 --name=mysql8 -e MYSQL_ROOT_PASSWORD= -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql:8
+#sudo docker create --restart unless-stopped -p 6379:6379 --name=redis redis
