7ddd918db4d0fd4ae72396ea13cc384f1f6d401f
motzel <motzel@o2.pl>
2024-06-06 23:00:34 +0200

    update dock icons

--- install/set-dock.sh
+++ install/set-dock.sh
@@ -1,2 +1,2 @@
 # Set default pinned applications (funny installer name to ensure it runs after all app installations!)
-gsettings set org.gnome.shell favorite-apps "['Alacritty.desktop', 'google-chrome.desktop', 'code.desktop', 'signal-desktop.desktop', 'typora.desktop', 'pinta_pinta.desktop', '1password.desktop', 'org.gnome.Settings.desktop', 'org.gnome.Nautilus.desktop']"
+gsettings set org.gnome.shell favorite-apps "['Alacritty.desktop', 'google-chrome.desktop', 'code.desktop', 'signal-desktop.desktop', 'org.gnome.Settings.desktop', 'org.gnome.Nautilus.desktop']"
